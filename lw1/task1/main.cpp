585aba29d2dc75e330c28f28380182cf46618799
Creado <ado-stalr@mail.ru>
2022-02-23 18:37:36 +0300

    внес правки в lw1 task1, вынес парс аргументов в функцию

--- lw1/task1/main.cpp
+++ lw1/task1/main.cpp
@@ -1,49 +1,90 @@
-#include <iostream>
 #include <fstream>
+#include <iostream>
+#include <optional>
 #include <string>
+#include <vector>
 
-void clearFile(const std::string& fileName)
+struct Args
 {
-	std::fstream clear_file(fileName, std::ios::out);
-	clear_file.close();
-}
+	std::vector<std::string> inputFileNames;
+	std::string outputFileName;
+};
 
-
-int main(int argc, char* argv[])
+std::optional<Args> ParseArgs(int argc, char* argv[])
 {
 	if (argc < 3)
 	{
 		std::cout << "Invalid argument count\n"
 				  << "Usage: join.exe <input file 1> ... <input file N> <output file>\n";
-		return 1;
+		return std::nullopt;
 	}
 
-	std::ofstream output(argv[argc - 1]);
-	if (!output.is_open())
-	{
-		std::cout << "Failed to open " << argv[argc - 1] << " for writing\n";
-		return 1;
-	}
-
+	Args args;
 	for (int i = 1; i < argc - 1; i++)
 	{
-		std::ifstream input(argv[i]);
+		args.inputFileNames.push_back(argv[i]);
+	}
+	args.outputFileName = argv[argc - 1];
+
+	return args;
+}
+
+void ClearFile(const std::string& fileName)
+{
+	std::fstream clear_file(fileName, std::ios::out);
+	clear_file.close();
+}
+
+void CopyFile(std::ifstream& input, std::ofstream& output)
+{
+	char ch;
+	while (input.get(ch))
+	{
+		if (!output.put(ch))
+		{
+			break;
+		}
+	}
+}
+
+int main(int argc, char* argv[])
+{
+	auto args = ParseArgs(argc, argv);
+	if (!args)
+	{
+		return 1;
+	}
+
+	std::ofstream output(args->outputFileName);
+	if (!output.is_open())
+	{
+		std::cout << "Failed to open " << args->outputFileName << " for writing\n";
+		return 1;
+	}
+
+	for (int i = 0; i < args->inputFileNames.size(); i++)
+	{
+		std::ifstream input(args->inputFileNames[i]);
 		if (!input.is_open())
 		{
-			std::cout << "Failed to open " << argv[1] << " for reading\n";
+			std::cout << "Failed to open " << args->inputFileNames[i] << " for reading\n";
 			output.close();
-			clearFile(std::string(argv[argc - 1]));
+			ClearFile(std::string(args->outputFileName));
 			return 1;
 		}
 
-		char ch;
-		while (input.get(ch))
+		CopyFile(input, output);
+
+		if (input.bad())
 		{
-			if (!output.put(ch))
-			{
-				std::cout << "Failed to save data on disk\n";
-				return 1;
-			}
+			std::cout << "Failed to read data from input file\n";
+			return 1;
+		}
+
+		if (!output.flush())
+		{
+			std::cout << "Failed to copy text to output file\n";
+			return 1;
 		}
 	}
 
